4fb29e68d8ee257d77e53cfa4f1dc571c6dd9ab2
Andreas AnZy Zymny <andreas@zymny.de>
2025-06-25 11:28:17 +0200

    Bug fix.

--- phi-calc/service-worker.js
+++ phi-calc/service-worker.js
@@ -2,26 +2,26 @@ const basePath = './';
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('phi-calc-v1').then(cache => {
+    caches.open('phi-calc').then(cache => {
       return cache.addAll([
-        ${basePath},
-        ${basePath} + 'index.html',
-        ${basePath} + 'index.js',
-        ${basePath} + 'math.js',
-        ${basePath} + 'math.js.map',
-        ${basePath} + 'styles.css',
-        ${basePath} + 'manifest.webmanifest',
-        ${basePath} + 'icons/icon-192.png',
-        ${basePath} + 'icons/icon-512.png',
-        ${basePath} + '../fonts/Audiowide/Audiowide-Regular.ttf',
-        ${basePath} + '../fonts/IBM_Plex_Sans/IBMPlexSans-VariableFont_wdth,wght.ttf',
-        ${basePath} + '../fonts/IBM_Plex_Serif/IBMPlexSerif-Regular.ttf',
-        ${basePath} + '../fonts/IBM_Plex_Sans_Condensed/IBMPlexSansCondensed-Regular.ttf',
-        ${basePath} + '../fonts/Saira/Saira-VariableFont_wdth,wght.ttf',
-        ${basePath} + '../fonts/Saira_Condensed/SairaCondensed-Regular.ttf',
-        ${basePath} + '../fonts/Saira_Extra_Condensed/SairaExtraCondensed-Regular.ttf',
-        ${basePath} + '../fonts/Saira_Semi_Condensed/SairaSemiCondensed-Regular.ttf',
-        ${basePath} + '../fonts/Saira_Stencil_One/SairaStencilOne-Regular.ttf'
+        basePath,
+        basePath + 'index.html',
+        basePath + 'index.js',
+        basePath + 'math.js',
+        basePath + 'math.js.map',
+        basePath + 'styles.css',
+        basePath + 'manifest.webmanifest',
+        basePath + 'icons/icon-192.png',
+        basePath + 'icons/icon-512.png',
+        basePath + '../fonts/Audiowide/Audiowide-Regular.ttf',
+        basePath + '../fonts/IBM_Plex_Sans/IBMPlexSans-VariableFont_wdth,wght.ttf',
+        basePath + '../fonts/IBM_Plex_Serif/IBMPlexSerif-Regular.ttf',
+        basePath + '../fonts/IBM_Plex_Sans_Condensed/IBMPlexSansCondensed-Regular.ttf',
+        basePath + '../fonts/Saira/Saira-VariableFont_wdth,wght.ttf',
+        basePath + '../fonts/Saira_Condensed/SairaCondensed-Regular.ttf',
+        basePath + '../fonts/Saira_Extra_Condensed/SairaExtraCondensed-Regular.ttf',
+        basePath + '../fonts/Saira_Semi_Condensed/SairaSemiCondensed-Regular.ttf',
+        basePath + '../fonts/Saira_Stencil_One/SairaStencilOne-Regular.ttf'
       ]);
     })
   );
